114a62da8f13387d166428ff39cab5f6c899e551
Tanmay12123 <mutalikdesaitanmay@gmail.com>
2020-12-16 20:20:44 +0530

    done with day_19 and some day_20 too!

--- Challange/myself/main.py
+++ Challange/myself/main.py
@@ -1,65 +1,12 @@
-from turtle import Turtle,Screen
-import random
-
-automatrix = Turtle()
-
-num = [0,1]
-
-
-colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
-def returner():
-    for i in range(4):
-        automatrix.color(random.choice(colours))
-        automatrix.pensize(10)
-        automatrix.left(90)
-        automatrix.forward(150)
-
-
-returner()
-def door():
-    automatrix.left(180)
-    automatrix.forward(45)
-    automatrix.right(90)
-    automatrix.forward(60)
-    automatrix.left(90)
-    automatrix.forward(60)
-    automatrix.left(90)
-    automatrix.forward(60)
-    automatrix.right(90)
-door()
-
-
-
-automatrix.forward(45)
-automatrix.right(90)
-automatrix.forward(150)
-automatrix.right(90)
-automatrix.forward(110)
-automatrix.left(65)
-automatrix.forward(80)
-# for i in range(4):
-#     automatrix.circle(10)
-#     automatrix.forward(2)
-
-
-automatrix.write("Automatrix")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-screen = Screen()
-screen.exitonclick()
+def tanmay():
+    """
+    just testing it out
+    """
+    def __init__(self, name):
+        self.name = name
+        
+    def tamy(self,numbers):
+        return numbers + 1
+    
+tmd = tanmay()
+tmd.tamy(23)
--- challange_6/main.py
+++ challange_6/main.py
@@ -1,55 +1,55 @@
 from turtle import Turtle, Screen
 import random
-is_race_on = False
+# is_race_on = False
 screen = Screen()
-screen.setup(width = 500,height = 400)
-user_bet = screen.textinput(title = "Make your bet!", prompt = "Who will win? Enter a color:  ")
-print(user_bet)
-colours = ["red","orange","green","blue","purple","yellow",]
-distance = random.randint(0,10)
+# screen.setup(width = 500,height = 400)
+# user_bet = screen.textinput(title = "Make your bet!", prompt = "Who will win? Enter a color:  ")
+# print(user_bet)
+# colours = ["red","orange","green","blue","purple","yellow",]
+# distance = random.randint(0,10)
 
 
-snape = Turtle(shape = "turtle")
-snape.penup()
-snape.color(colours[0])
-snape.goto(x = -230,y = -100)
-while is_race_on:
-    snape.forward(distance)
+# snape = Turtle(shape = "turtle")
+# snape.penup()
+# snape.color(colours[0])
+# snape.goto(x = -230,y = -100)
+# while is_race_on:
+#     snape.forward(distance)
 
-harry = Turtle(shape = "turtle")
-harry.penup()
-harry.color(colours[1])
-harry.goto(x = -230,y = -50)
-while is_race_on:
-    harry.forward(distance)
+# harry = Turtle(shape = "turtle")
+# harry.penup()
+# harry.color(colours[1])
+# harry.goto(x = -230,y = -50)
+# while is_race_on:
+#     harry.forward(distance)
 
-ron = Turtle(shape = "turtle")
-ron.penup()
-ron.color(colours[2])
-ron.goto(x = -230,y = 0)
-while is_race_on:
-    ron.forward(distance)
+# ron = Turtle(shape = "turtle")
+# ron.penup()
+# ron.color(colours[2])
+# ron.goto(x = -230,y = 0)
+# while is_race_on:
+#     ron.forward(distance)
 
-dumble = Turtle(shape = "turtle")
-dumble.penup()
-dumble.color(colours[3])
-dumble.goto(x = -230,y = 50)
-while is_race_on:
-    dumble.forward(distance)
+# dumble = Turtle(shape = "turtle")
+# dumble.penup()
+# dumble.color(colours[3])
+# dumble.goto(x = -230,y = 50)
+# while is_race_on:
+#     dumble.forward(distance)
 
-herm = Turtle(shape = "turtle")
-herm.penup()
-herm.color(colours[4])
-herm.goto(x = -230,y = 100)
-while is_race_on:
-    herm.forward(distance)
+# herm = Turtle(shape = "turtle")
+# herm.penup()
+# herm.color(colours[4])
+# herm.goto(x = -230,y = 100)
+# while is_race_on:
+#     herm.forward(distance)
 
-levis = Turtle(shape = "turtle")
-levis.penup()
-levis.color(colours[5])
-levis.goto(x = -230,y = 150)
-while is_race_on:
-    levis.forward(distance)
+# levis = Turtle(shape = "turtle")
+# levis.penup()
+# levis.color(colours[5])
+# levis.goto(x = -230,y = 150)
+# while is_race_on:
+#     levis.forward(distance)
 
 
 
@@ -95,3 +95,40 @@ while is_race_on:
 
 # movements()
 screen.exitonclick()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
