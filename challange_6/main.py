e456daa00c7b65544f447e133e0dc0f7a99e92f6
Tanmay12123 <mutalikdesaitanmay@gmail.com>
2020-12-16 13:21:30 +0530

    done with turtle race

---
+++ challange_6/main.py
@@ -0,0 +1,97 @@
+from turtle import Turtle, Screen
+import random
+is_race_on = False
+screen = Screen()
+screen.setup(width = 500,height = 400)
+user_bet = screen.textinput(title = "Make your bet!", prompt = "Who will win? Enter a color:  ")
+print(user_bet)
+colours = ["red","orange","green","blue","purple","yellow",]
+distance = random.randint(0,10)
+
+
+snape = Turtle(shape = "turtle")
+snape.penup()
+snape.color(colours[0])
+snape.goto(x = -230,y = -100)
+while is_race_on:
+    snape.forward(distance)
+
+harry = Turtle(shape = "turtle")
+harry.penup()
+harry.color(colours[1])
+harry.goto(x = -230,y = -50)
+while is_race_on:
+    harry.forward(distance)
+
+ron = Turtle(shape = "turtle")
+ron.penup()
+ron.color(colours[2])
+ron.goto(x = -230,y = 0)
+while is_race_on:
+    ron.forward(distance)
+
+dumble = Turtle(shape = "turtle")
+dumble.penup()
+dumble.color(colours[3])
+dumble.goto(x = -230,y = 50)
+while is_race_on:
+    dumble.forward(distance)
+
+herm = Turtle(shape = "turtle")
+herm.penup()
+herm.color(colours[4])
+herm.goto(x = -230,y = 100)
+while is_race_on:
+    herm.forward(distance)
+
+levis = Turtle(shape = "turtle")
+levis.penup()
+levis.color(colours[5])
+levis.goto(x = -230,y = 150)
+while is_race_on:
+    levis.forward(distance)
+
+
+
+
+
+
+
+
+
+# def forward():
+#     snape.forward(10)
+
+
+# def backward():
+#     snape.backward(10)
+
+
+# def left():
+#     snape.left(90)
+#     snape.forward(10)
+
+
+# def right():
+#     snape.right(90)
+#     snape.forward(10) 
+    
+# def clear():
+#     snape.reset()
+
+# screen.listen()
+
+
+# def keys(keys, func):
+#     screen.onkey(key=keys, fun=func)
+
+# def movements():
+#     keys("w", forward)
+#     keys("a", left)
+#     keys("s", backward)
+#     keys("d", right)
+#     keys("c",clear)
+
+
+# movements()
+screen.exitonclick()
